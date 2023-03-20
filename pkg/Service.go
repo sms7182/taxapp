56e35c99aedf7a33519bee47ba4cdce9a0a5767b
Mehdi Mousavi <mehdi.mousavi@snapptrip.com>
2023-03-20 19:20:14 +0330

    check date is after 1 farvardin 1402.

--- pkg/Service.go
+++ pkg/Service.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"errors"
 	"tax-management/external"
+	"time"
 )
 
 type Service struct {
 	Repository Repository
 	TaxClient  map[string]TaxClient
 }
+
+const layout = "2006-01-02T15:04:05"
 
 func (s Service) ProcessKafkaMessage(topicName string, data external.RawTransaction) error {
 	taxId, taxProcessId, e := s.Repository.InsertTaxData(context.Background(), topicName, data)
@@ -18,6 +21,12 @@ func (s Service) ProcessKafkaMessage(topicName string, data external.RawTransact
 	}
 
 	println("data")
+
+	farvardin1, _ := time.Parse(layout, "2023-03-20T23:59:59")
+
+	if time.UnixMicro(data.After.Indatim).Before(farvardin1) {
+		return nil
+	}
 
 	if client, ok := s.TaxClient[data.After.Taxid]; ok {
 		invoice := data.ToStandardInvoice()
