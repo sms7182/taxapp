335e90c5c45b1c62679619e139a6b726d24f2671
sms7182 <sms7182@gitlab.com>
2023-12-12 08:25:12 +0330

    refactor for saving

---
+++ Models/Enums/IntyType.cs
@@ -0,0 +1,10 @@
+using System;
+namespace SendTaxDataApp.Models.Enums{
+public enum IntyType{
+     [System.ComponentModel.Description("نوع اول")]
+    First=1,
+     [System.ComponentModel.Description("نوع دوم")]
+    Second=2,
+     [System.ComponentModel.Description("نوع سوم")]
+    Third=3
+}}
--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -7,6 +7,7 @@ public class Invoice{
 public int Setm { get; set; }
 public int Inp { get; set; }
 public int Ins { get; set; }
+public int Inty { get; set; }
 public string Tins { get; set; }    
 public string Tinb { get; set; }
 public decimal Tadis { get; set; }
@@ -17,6 +18,10 @@ public decimal Tprdis { get; set; }
 public decimal Todam { get; set; }
 public decimal Tdis {get;set;}
 public decimal Cap { get; set; }  
+private DateTime invoiceDate;
+public double Indatim{get;set;}
+public string Username { get; set; }="A16XAX";
+
 public List<InvoiceItem> Detail{get;set;}
 }
 }
